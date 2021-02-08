f619706dcd6df97ee0242605beaeb37744a6575f
Duraan Ali <duraan522@gmail.com>
2021-02-07 19:39:24 -0600

    first commit

---
+++ src/data.js
@@ -0,0 +1,30 @@
+export default [
+	{
+		id: 1,
+		title: 'The Art Of War',
+		price: 10.11,
+		image:
+			'https://tm.gabischool.com/assets/themes/admin/images/TM/section_two/week_three/day_one/41FBMkY3cgL._SX331_BO1,204,203,200_.jpg'
+	},
+	{
+		id: 2,
+		title: 'Civilization and Its Discontents',
+		price: 14.29,
+		image:
+			'https://tm.gabischool.com/assets/themes/admin/images/TM/section_two/week_three/day_one/51Jj12iMZnL._SX331_BO1,204,203,200_.jpg'
+	},
+	{
+		id: 3,
+		title: 'Influence: The Psychology of Persuasion',
+		price: 10.98,
+		image:
+			'https://tm.gabischool.com/assets/themes/admin/images/TM/section_two/week_three/day_one/512-B-1yXuL._SX331_BO1,204,203,200_.jpg'
+	},
+	{
+		id: 4,
+		title: 'Grokking Algorithms',
+		price: 40.00,
+		image:
+			'https://tm.gabischool.com/assets/themes/admin/images/TM/section_two/week_three/day_one/61uUPXbhMxL._SX397_BO1,204,203,200_.jpg'
+	}
+];
