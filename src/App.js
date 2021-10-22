de48d966fe7e52e85c3fcc8d3455cf99be3a3aeb
cayto <aidarous1143@gmail.com>
2021-10-22 17:03:48 +0300

    MPV

--- src/App.js
+++ src/App.js
@@ -1,32 +1,46 @@
-import React, { useState } from 'react';
+import React, { useState} from 'react';
 import { Route } from 'react-router-dom';
 import data from './data';
 
+// Style 
+import './sass/index.scss'
 // Components
 import Navigation from './components/Navigation';
 import Products from './components/Products';
 import ShoppingCart from './components/ShoppingCart';
+//context 
+import { ProductContext } from './contexts/ProductContext';
+import { CartContext} from './contexts/CartContext';
+
 
 function App() {
 	const [products] = useState(data);
 	const [cart, setCart] = useState([]);
+	console.log(!cart);
 
 	const addItem = item => {
 		// add the given item to the cart
+		if(!cart.find(cardItem => cardItem.id === item.id)){
+			setCart([...cart, item])
+		}
 	};
 
 	return (
 		<div className="App">
-			<Navigation cart={cart} />
+			<ProductContext.Provider value={{ products, addItem }}>
+				<CartContext.Provider value={cart}>
+					<Navigation />
 
-			{/* Routes */}
-			<Route exact path="/">
-				<Products products={products} addItem={addItem} />
-			</Route>
+					{/* Routes */}
+					<Route exact path="/">
+						<Products />
+					</Route>
 
-			<Route path="/cart">
-				<ShoppingCart cart={cart} />
-			</Route>
+					<Route path="/cart">
+						<ShoppingCart />
+					</Route>
+				</CartContext.Provider>
+			</ProductContext.Provider>
 		</div>
 	);
 }
